058b9b3ea04b3329b9b887768108d3e8a57382fb
Mahijasoliya17 <mahijasoliya04@gmail.com>
2025-07-30 16:13:35 +0530

    updated

--- Django_cont/icoder/blog/templatetags/extras.py
+++ Django_cont/icoder/blog/templatetags/extras.py
@@ -1,5 +1,31 @@
+# from django import template
+# from home.models import UserProfile
+# register = template.Library()
+
+# @register.filter
+# def get_item(dictionary, key, user):
+#     if dictionary is None:
+#         return None
+#     return dictionary.get(key)
+
+# def get_profile_pic(user):
+#     try:
+#         return user.userprofile.profile_pic.url
+#     except:
+#         return '/media/profile_pics/default_user.png'  # fallback if none
+
+
 from django import template
+from home.models import UserProfile
+
 register = template.Library()
+
+@register.filter
+def get_profile_pic(user):
+    try:
+        return user.userprofile.profile_pic.url
+    except:
+        return '/media/profile_pics/default_user.png'  # fallback
 
 @register.filter
 def get_item(dictionary, key):
